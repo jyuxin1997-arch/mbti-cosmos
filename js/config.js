9491d2428cab79f39d231a0f2bab9405647c3833
jiangyux <jyuxin1997@gmail.com>
2026-05-20 22:33:47 +0800

    config: 接入 football-data.org API Token

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 window.AppConfig = {
   SUPABASE_URL: 'https://vurbnizpicrfxttclnza.supabase.co',
   SUPABASE_ANON_KEY: 'sb_publishable_Jk0dl4ZO05xKNkr70pFuTg_TliLkt6Z',
-  FOOTBALL_API_TOKEN: '',  // 用户在 football-data.org 注册后填入
+  FOOTBALL_API_TOKEN: '3738c313a5874659b40ec60e73ae038f',
   SITE_URL: 'https://jyuxin1997-arch.github.io/mbti-cosmos/',
   API_POLL_INTERVAL: 300000, // 5分钟轮询
   API_CACHE_TTL: 300000,    // 5分钟缓存
